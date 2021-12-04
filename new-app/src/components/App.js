c3df3b87bf0204109544ca86334f9c6fdf7db1d9
RaghavMaskara21 <raghavmaskara@gmail.com>
2021-12-05 01:02:23 +0530

    added dark header

--- new-app/src/components/App.js
+++ new-app/src/components/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import Header from './Bootstrap/header';
+
+
 
  class App extends Component {
   render() {
     return (
       <div className="app">
         <h1>Raghav</h1>
-        
+        <Header>Raghav's new app</Header>
+         <Header dark={true}>Raghav's new app</Header>
       </div>
     )
   }
